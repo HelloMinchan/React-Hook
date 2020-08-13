d7a4d07861d236a60aba11d8017c8a6edf1fa5a3
hellominchan <wjdalscksdle@naver.com>
2020-08-13 16:41:23 +0900

    useState작성2

--- src/pages/Usestate.tsx
+++ src/pages/Usestate.tsx
@@ -2,6 +2,16 @@ import React, { useState } from "react";
 
 function Usestate() {
   const [value, setValue] = useState(0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputEmail = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handleInputPassword = (e: any) => {
+    setPassword(e.target.value);
+  };
 
   return (
     <div>
@@ -11,6 +21,30 @@ function Usestate() {
       </p>
       <button onClick={() => setValue(value + 1)}>+다람쥐</button>
       <button onClick={() => setValue(value - 1)}>-다람쥐</button>
+
+      <hr />
+
+      <div>
+        <input
+          value={email}
+          onChange={handleInputEmail}
+          placeholder={"이메일이다람쥐"}
+        />
+        <input
+          value={password}
+          onChange={handleInputPassword}
+          placeholder={"비밀번호다람쥐"}
+        />
+      </div>
+
+      <div>
+        <b>이메일:</b>
+        {email}
+      </div>
+      <div>
+        <b>비밀번호:</b>
+        {password}
+      </div>
     </div>
   );
 }
